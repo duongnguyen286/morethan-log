3d430bd7c55fa8c4b1298907ee539fa6b0fa08cd
Nguyễn Tùng Dương <39269760+duongnguyen286@users.noreply.github.com>
2023-10-25 17:29:28 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "duongnguyen286",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "@gmail.com",
+    linkedin: "duongnguyen286",
+    github: "duongnguyen286",
     instagram: "",
   },
   projects: [
